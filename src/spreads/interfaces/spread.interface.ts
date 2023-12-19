a8ff1f7f695064948a036f667f2aac99e408a8bd
Ignacio Díaz <ignacio.db@icodesx.com>
2023-12-19 19:25:50 -0300

    Updated spread interface file structure

--- src/spreads/interfaces/spread.interface.ts
+++ src/spreads/interfaces/spread.interface.ts
@@ -1,3 +1,15 @@
+/**
+ * Dependencies
+ * We must include all third-party libraries that we use in the project
+ * and are needed for this feature
+ */
+
+/**
+ * Sub-dependencies
+ * We must include all libraries developed within the project
+ * and are needed for this feature
+ */
+
 export interface Spread {
   id: string;
   market_id: string;
