4916220285e86cd2ff0fb41ef45b7ba68e3dcc3d
Ignacio Díaz <ignacio.db@icodesx.com>
2023-12-19 02:30:55 -0300

    Added spreads controller get market_id spread handler

---
+++ src/spreads/spreads.controller.ts
@@ -0,0 +1,24 @@
+/**
+ * Dependencies
+ * We must include all third-party libraries that we use in the project
+ * and are needed for this feature
+ */
+import { Controller, Get, Param, Version } from '@nestjs/common';
+
+/**
+ * Sub-dependencies
+ * We must include all libraries developed within the project
+ * and are needed for this feature
+ */
+import { SpreadsService } from './spreads.service';
+
+@Controller('spreads')
+export class SpreadsController {
+  constructor(private readonly spreadsService: SpreadsService) {}
+
+  @Get(':market_id')
+  @Version('1')
+  findOne(@Param('market_id') market_id: string): Promise<object> {
+    return this.spreadsService.findOne(market_id);
+  }
+}
