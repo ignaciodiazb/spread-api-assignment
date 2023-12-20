55782b586f5b68c964d39f11846664c3242a7e1c
Ignacio Díaz <ignacio.db@icodesx.com>
2023-12-20 13:33:37 -0300

    Updated spreads controller testing cases

--- src/spreads/spreads.controller.spec.ts
+++ src/spreads/spreads.controller.spec.ts
@@ -3,14 +3,17 @@
  * We must include all third-party libraries that we use in the project
  * and are needed for this feature
  */
-import { HttpModule, HttpService } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
 
 /**
  * Sub-dependencies
  * We must include all libraries developed within the project
  * and are needed for this feature
  */
+import { CreateSpreadAlertDto } from './dto/create-spread-alert.dto';
 import { SpreadsController } from './spreads.controller';
 import { SpreadsService } from './spreads.service';
 
@@ -27,7 +30,42 @@ describe('SpreadsController', () => {
     controller = module.get<SpreadsController>(SpreadsController);
   });
 
-  it('should be defined', () => {
+  it('(spreadController) should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('*create spread alert) - should be defined', () => {
+    expect(controller.createAlert).toBeDefined();
+  });
+
+  it('(create spread alert) - should throw validation errors with malformed body', async () => {
+    const mockBadRequestBody: CreateSpreadAlertDto = {
+      id: 'abc',
+      market_id: null,
+      max_bid: ['36352219.0', 'CLP'],
+      min_ask: ['36776580.99', 'CLP'],
+      spread: 424361.99,
+    };
+
+    const transformMock = plainToInstance(
+      CreateSpreadAlertDto,
+      mockBadRequestBody,
+    );
+
+    const validation = await validate(transformMock);
+
+    expect(validation.length).not.toBe(0);
+  });
+
+  it('(get all spreads) - should be defined', () => {
+    expect(controller.findAll).toBeDefined();
+  });
+
+  it('(get single spread) - should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('(get spread alert status) - should be defined', () => {
     expect(controller).toBeDefined();
   });
 });
